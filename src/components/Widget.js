aaba898a737c6ef2c17ae58e9d0288b9ab2de806
James Arnold <j1mes@hotmail.co.uk>
2021-12-15 10:36:02 +0000

    final commit

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -1,5 +1,3 @@
-//font only import from css.
-import "../App.css";
 //imgs
 import logoWhiteImg from "../assets/logo-white.svg";
 import logoGreenImg from "../assets/logo-green.svg";
@@ -27,126 +25,109 @@ const Widget = ({ widgetNum, fetchedData }) => {
 
   return (
     <WidgetStyled>
-      {fetchedData.length > 1 && (
-        <section className="widget-container">
-          <div className="widget-header">
-            <div className="widget-logo-col">
-              {theme === "whiteTheme" ? (
-                <img
-                  src={logoGreenImg}
-                  alt="greenspark logo green"
-                  className="GS-logo"
-                />
-              ) : theme === "beigeTheme" ? (
-                <img
-                  src={logoGreenImg}
-                  alt="greenspark logo green"
-                  className="GS-logo"
-                />
-              ) : (
-                <img
-                  src={logoWhiteImg}
-                  alt="greenspark logo white"
-                  className="GS-logo"
-                />
-              )}
-            </div>
-            <div className="widget-text-col">
-              {widgetNum === "1" && (
-                <div>
-                  <h3 className="widget-small-text">This product collects</h3>
-                  <h2 className="widget-big-text">
-                    {fetchedData[0].amount} {fetchedData[0].type}
-                  </h2>
-                </div>
-              )}
-              {widgetNum === "2" && (
-                <div>
-                  <h3 className="widget-small-text">This product plants</h3>
-                  <h2 className="widget-big-text">
-                    {fetchedData[1].amount} {fetchedData[1].type}
-                  </h2>
-                </div>
-              )}
-              {widgetNum === "3" && (
-                <div>
-                  <h3 className="widget-small-text">This product offsets</h3>
-                  <h2 className="widget-big-text">
-                    {fetchedData[2].amount}kgs of {fetchedData[2].type}
-                  </h2>
-                </div>
-              )}
-            </div>
+      <section className="widget-container">
+        <div className="widget-header">
+          <div className="widget-logo-col">
+            {theme === "whiteTheme" ? (
+              <img
+                src={logoGreenImg}
+                alt="greenspark logo green"
+                className="GS-logo"
+              />
+            ) : theme === "beigeTheme" ? (
+              <img
+                src={logoGreenImg}
+                alt="greenspark logo green"
+                className="GS-logo"
+              />
+            ) : (
+              <img
+                src={logoWhiteImg}
+                alt="greenspark logo white"
+                className="GS-logo"
+              />
+            )}
           </div>
-          <div className="widget-body">
-            <div className="widget-row">
-              <span className="widget-list-item">
-                Link to Public Profile
-                <TooltipDiv />
-              </span>
-
-              {!checkbox ? (
-                <img
-                  src={checkboxUntickImg}
-                  alt="checkbox"
-                  className="checkbox"
-                  onClick={() => setCheckbox(!checkbox)}
-                />
-              ) : (
-                <img
-                  src={checkboxTickImg}
-                  alt="checkbox"
-                  className="checkbox"
-                  onClick={() => setCheckbox(!checkbox)}
-                />
-              )}
-            </div>
-
-            <div className="widget-row">
-              <span>Badge colour</span>
-              <SwatchContainer>
-                <SwatchButton
-                  id="blueTheme"
-                  color="#2E3A8C"
-                  theme={theme}
-                  onClick={() => setTheme("blueTheme")}
-                />
-                <SwatchButton
-                  id="greenTheme"
-                  color="#3B755F"
-                  theme={theme}
-                  onClick={() => setTheme("greenTheme")}
-                />
-                <SwatchButton
-                  id="beigeTheme"
-                  color="#F2EBDB"
-                  theme={theme}
-                  onClick={() => setTheme("beigeTheme")}
-                />
-                <SwatchButton
-                  id="whiteTheme"
-                  color="#FFFFFF"
-                  theme={theme}
-                  onClick={() => setTheme("whiteTheme")}
-                />
-                <SwatchButton
-                  id="blackTheme"
-                  color="#212121"
-                  theme={theme}
-                  onClick={() => setTheme("blackTheme")}
-                />
-              </SwatchContainer>
-            </div>
-            <div className="widget-row">
-              <span>Activate badge</span>
-              <label className="switch">
-                <input type="checkbox" onClick={() => setToggle(!toggle)} />
-                <span className="slider round"></span>
-              </label>
-            </div>
+          <div className="widget-text-col">
+            {widgetNum === "1" && (
+              <div>
+                <h3 className="widget-small-text">This product collects</h3>
+                <h2 className="widget-big-text">
+                  {/* API data would go here if active: {fetchedData[0].amount} {fetchedData[0].type} */}
+                  300 Plastic Bottles
+                </h2>
+              </div>
+            )}
           </div>
-        </section>
-      )}
+        </div>
+        <div className="widget-body">
+          <div className="widget-row">
+            <span className="widget-list-item">
+              Link to Public Profile
+              <TooltipDiv />
+            </span>
+
+            {!checkbox ? (
+              <img
+                src={checkboxUntickImg}
+                alt="checkbox"
+                className="checkbox"
+                onClick={() => setCheckbox(!checkbox)}
+              />
+            ) : (
+              <img
+                src={checkboxTickImg}
+                alt="checkbox"
+                className="checkbox"
+                onClick={() => setCheckbox(!checkbox)}
+              />
+            )}
+          </div>
+
+          <div className="widget-row">
+            <span>Badge colour</span>
+            <SwatchContainer>
+              <SwatchButton
+                id="blueTheme"
+                color="#2E3A8C"
+                theme={theme}
+                onClick={() => setTheme("blueTheme")}
+              />
+              <SwatchButton
+                id="greenTheme"
+                color="#3B755F"
+                theme={theme}
+                onClick={() => setTheme("greenTheme")}
+              />
+              <SwatchButton
+                id="beigeTheme"
+                color="#F2EBDB"
+                theme={theme}
+                onClick={() => setTheme("beigeTheme")}
+              />
+              <SwatchButton
+                id="whiteTheme"
+                color="#FFFFFF"
+                theme={theme}
+                onClick={() => setTheme("whiteTheme")}
+              />
+              <SwatchButton
+                id="blackTheme"
+                color="#212121"
+                theme={theme}
+                onClick={() => setTheme("blackTheme")}
+              />
+            </SwatchContainer>
+          </div>
+          <div className="widget-row">
+            <span>Activate badge</span>
+            <label className="switch">
+              <input type="checkbox" onClick={() => setToggle(!toggle)} />
+              <span className="slider round"></span>
+            </label>
+          </div>
+        </div>
+      </section>
     </WidgetStyled>
   );
 };
